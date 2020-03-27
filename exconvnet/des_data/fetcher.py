88071103a4ffbc1e0c6542031d4bb7ac73d386d2
Rodrigo Castellon <rjcaste@stanford.edu>
2020-03-27 16:41:42 -0400

    added saving to fetch() and wrote out ExConvDataset class

--- exconvnet/des_data/fetcher.py
+++ exconvnet/des_data/fetcher.py
@@ -1,3 +1,7 @@
+import time
+import numpy as np
+import os
+
 from .downloader import download
 from .processing import process_X, gen_labels
 from .fetching_utils import sightlines2links, gen_sightlines
@@ -5,11 +9,11 @@ from .filter import DefaultFilter
 
 __all__ = ['fetch']
 
-def fetch(sightlines=None, cols=None, filters=None, gen_Y=True, verbose=True):
+def fetch(sightlines=None, cols=None, filters=None, save_path=None, gen_Y=True, verbose=True):
     """User-level method to do end-to-end fetching of DES data to
     inputs for train/val/test. Give a grid of k sightlines to return
     k examples, the columns you would like to keep, and the filters
-    to use.
+    to use. Automatically saves the datasets to a `datasets` folder.
 
     sightlines : np.ndarray
         A (k, 2) array where each row is a sightline
@@ -17,6 +21,8 @@ def fetch(sightlines=None, cols=None, filters=None, gen_Y=True, verbose=True):
         List of columns to include; each element is a non-negative integer
     filters : list
         A list of names for filter functions to use (see filter.py for the filter function names)
+    save_path : str
+        path to save the fetched dataset to
     gen_Y : bool
         Should automatically generate the y-labels for X
     verbose : bool
@@ -34,6 +40,10 @@ def fetch(sightlines=None, cols=None, filters=None, gen_Y=True, verbose=True):
     # interpret the user inputs
     if sightlines is None:
         sightlines = gen_sightlines()
+    if save_path is None:
+        exconvnet_dir = os.path.split(os.path.split(os.path.realpath(__file__))[0])[0]
+        save_path = os.path.join(exconvnet_dir, 'datasets')
+
     filter_obj = DefaultFilter(cols=cols, filters=filters)
     links = sightlines2links(sightlines)
 
@@ -58,6 +68,16 @@ def fetch(sightlines=None, cols=None, filters=None, gen_Y=True, verbose=True):
 
         if verbose:
             print('generated labels for X')
+
+    # pickle/save them
+    if not(os.path.exists(save_path)):
+        os.mkdir(save_path)
+
+    tag = str(round(time.time()))
+    x_fname = tag + '_X.npy'
+    y_fname = tag + '_Y.npy'
+    np.save(os.path.join(save_path, x_fname), X)
+    np.save(os.path.join(save_path, y_fname), Y)
 
     return X, Y
 
--- exconvnet/trainval_data/xy_data.py
+++ exconvnet/trainval_data/xy_data.py
@@ -8,27 +8,40 @@ class ExConvDataset(Dataset):
     """Represents the x/y data used to train or validate our models
 
     """
-    def __init__(self, ):
+    def __init__(self, x_path, y_path, data_cfg):
         """
         Parameters
         ----------
-        
-
+        x_path : str
+            path to the .npy file containing X
+        y_path : str
+            path to the .npy file containing Y
         """
-        # get 
-        pass
+        # get X and Y loaded
+        self.X = np.load(x_path, allow_pickle=True)
+        self.Y = np.load(y_path, allow_pickle=True)
+
+        self.n_data = self.X.shape[0]
     def __len__(self):
-        pass
+        return self.n_data
     def __getitem__(self, idx):
-        pass
+        return torch.FloatTensor(self.X[idx]), torch.FloatTensor(self.Y[idx])
 
 class ExConvTestSet(dataset):
     """Represents the XData used to test our models
 
     """
-    def __init__(self):
-        pass
+    def __init__(self, x_path):
+        """
+        Parameters
+        ----------
+        x_path : str
+            path to the .npy file containing X
+        """
+        self.X = np.load(x_path, allow_pickle=True)
+
+        self.n_data = self.X.shape[0]
     def __len__(self):
-        pass
+        return self.n_data
     def __getitem__(self, idx):
-        pass
+        return self.X[idx]
